c6d2e9760dbfb6c630b17b1c6eaa0a18ec0d1ea7
brandon <brandonyeoxg@gmail.com>
2023-11-25 16:38:04 +0800

    feat: init

--- consistenthash/consistenthash.go
+++ consistenthash/consistenthash.go
@@ -1,19 +1,42 @@
 package consistenthash
 
-const (
-	maxWeight   = 100
-	minReplicas = 100
+import (
+	"sync"
 
-	prime = 359445410222021406858304963543
+	"github.com/spaolacci/murmur3"
 )
 
-type HashFn func([]byte) []byte
+const (
+	maxWeight       = 100
+	minReplicaCount = 100
+
+	prime = 359445410222021406858304963543 // random prime number
+)
+
+type HashFn func([]byte) uint64
 
 type ConsistentHash struct {
 	hashFn HashFn
 
-	replicaCount uint64
-	weight       uint32
-	ring         []uint64
-	nodeLookup   map[uint64]any
+	replicaCount uint
+
+	mut        sync.RWMutex
+	keys       []uint64
+	ring       map[uint64][]any
+	nodeLookup map[uint64]any
+}
+
+func New(opts ...Option) *ConsistentHash {
+	ch := &ConsistentHash{
+		hashFn:     murmur3.Sum64,
+		keys:       make([]uint64, 0),
+		ring:       make(map[uint64][]any),
+		nodeLookup: make(map[uint64]any),
+	}
+
+	for _, opt := range opts {
+		opt(ch)
+	}
+
+	return ch
 }
--- consistenthash/option.go
+++ consistenthash/option.go
@@ -1,1 +1,18 @@
 package consistenthash
+
+type Option func(*ConsistentHash)
+
+func SetReplicas(count uint64) Option {
+	return func(ch *ConsistentHash) {
+		if count < minReplicaCount {
+			count = minReplicaCount
+		}
+		ch.replicaCount = uint(count)
+	}
+}
+
+func SetHashFn(fn HashFn) Option {
+	return func(ch *ConsistentHash) {
+		ch.hashFn = fn
+	}
+}
